46c59c9fac7354e5b7947748ccac2ee1327ec326
Lukas A. Mueller <lukemueller@protonmail.com>
2018-01-01 21:38:12 +0100

    Prepare for coding

---
+++ Kana/KanaRomanizationController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kana
+//
+//  Created by Luke Morgenstern on 01.01.18.
+//  Copyright © 2018 Lukas A. Mueller. All rights reserved.
+//
+
+import UIKit
+
+class KanaRomanizationController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .white
+  }
+
+}
+
